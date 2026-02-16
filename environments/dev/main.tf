46990f0f19f5224235fb2e78bd3888757af17def
felipesalesoliveira <fesales.oliveira@gmail.com>
2026-02-16 16:13:30 -0300

    feat: criação module networking

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,15 +1,15 @@
 module "vpc_app" {
   source = "../../modules/vpc"
 
-  name   = "vpc-app"
-  cidr   = "10.0.0.0/16"
-  azs    = var.azs
+  name = "vpc-app"
+  cidr = "10.0.0.0/16"
+  azs  = var.azs
 }
 
 module "vpc_obs" {
   source = "../../modules/vpc"
 
-  name   = "vpc-observability"
-  cidr   = "10.1.0.0/16"
-  azs    = var.azs
+  name = "vpc-observability"
+  cidr = "10.1.0.0/16"
+  azs  = var.azs
 }
